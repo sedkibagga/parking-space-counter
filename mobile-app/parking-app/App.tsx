3efd241d9469af7ebafeda84d2a65e14edfdf917
sedkibagga <153396601+sedkibagga@users.noreply.github.com>
2025-01-26 17:15:50 +0100

    first touch after pulling

--- mobile-app/parking-app/App.tsx
+++ mobile-app/parking-app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hi guys </Text>
+      <Text>hi bro </Text>
       <StatusBar style="auto" />
     </View>
   );
