d85a1ce9c14795a996eaee3bee8917465a6f3543
sedkibagga <153396601+sedkibagga@users.noreply.github.com>
2025-02-12 02:06:49 +0100

    setup-tailwin-css

---
+++ web-app/parking-web-app/src/component/TestComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TestComponent() {
+  return (
+    <div>
+       <p className=' text-3xl text-white font-bold'>
+          yallla
+        </p>
+        
+    </div>
+  )
+}
+
+export default TestComponent
