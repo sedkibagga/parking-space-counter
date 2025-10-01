567a5ec45a3caa528618276f3a6258b36de82db4
sedkibagga <153396601+sedkibagga@users.noreply.github.com>
2025-10-01 17:07:01 +0100

    Update JwtService.java

--- backend/springboot/src/main/java/com/bagga/springboot/config/JwtService.java
+++ backend/springboot/src/main/java/com/bagga/springboot/config/JwtService.java
@@ -19,29 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class JwtService {
-//    private static final Dotenv dotenv = Dotenv.configure()
-//            .directory("C:\\Users\\Dell\\Desktop\\The Future of Parking\\backend\\springboot\\src\\main\\resources")
-//            .filename(".env")
-//            .load();
-//
-//    private static final String SECRET_KEY = dotenv.get("SECRET_KEY");
-//    public JwtService() {
-//
-////        log.info("SECRET_KEY: {}", dotenv.get("SECRET_KEY"));
-//        log.info("SECRET_KEY: {}", SECRET_KEY);
-//    }
-
-//    private static final Dotenv dotenv = Dotenv.configure()
-//            .directory("/home/app") // Path to the .env file inside the container
-//            .filename(".env")
-//            .load();
-//
-//    private static final String SECRET_KEY = dotenv.get("SECRET_KEY");
-//
-//    public JwtService() {
-//
-//        log.info("SECRET_KEY: {}", dotenv.get("SECRET_KEY"));
-//    }
 
     private static final String SECRET_KEY = System.getenv("SECRET_KEY");
 
